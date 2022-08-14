2cbfc6a425d7f704963a168480f30fe197f37dfe
Tanqueray <d.takaki1010@gmail.com>
2022-08-15 01:21:38 +0900

    Add 5 members Model and show their firstname

--- members/views.py
+++ members/views.py
@@ -2,11 +2,19 @@ from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 # Create your views here.
 # def index(request):
 #    return HttpResponse("Hello world!")
 
+# def index(request):
+#     template = loader.get_template('myfirst.html')
+#     return HttpResponse(template.render())
+
 def index(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Members.objects.all().values()
+    output = "" # define empty output
+    for x in mymembers:
+        output += x["firstname"] # Members model have firstname and lastname fields
+    return HttpResponse(output)
