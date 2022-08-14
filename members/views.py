8fff95138a6f4c3f62d39958dc7eb70fc10330dc
Tanqueray <d.takaki1010@gmail.com>
2022-08-15 00:19:24 +0900

    before migrate.Be carafull donot runserver until myworld/setting.py modified

--- members/views.py
+++ members/views.py
@@ -1,6 +1,12 @@
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+# def index(request):
+#    return HttpResponse("Hello world!")
+
 def index(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
